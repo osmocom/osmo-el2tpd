ea517a5c3fe5a60ae8115a0f707d48b47a2e98ec
Alexander Couzens <lynxis@fe80.eu>
2016-10-24 21:35:02 +0200

    l2tpd_fsm: answer to a to STOP CCN with an ACK
    still a TODO to tear down the fsm and struct

--- siu/l2tp/l2tpd_fsm.c
+++ siu/l2tp/l2tpd_fsm.c
@@ -23,17 +23,24 @@ static void l2tp_ctrl_s_wait_ctl_conn(struct osmo_fsm_inst *fi, uint32_t event, 
 		l2tp_tx_ack(l2c);
 		osmo_fsm_inst_state_chg(fi, L2CC_S_ESTABLISHED, 0, 0);
 		break;
+	case L2CC_E_RX_STOP_CCN:
+		l2tp_tx_ack(l2c);
+		/* FIXME: tear down whole l2c */
+		break;
 	}
 }
 
 static void l2tp_ctrl_s_established(struct osmo_fsm_inst *fi, uint32_t event, void *data)
 {
+	struct l2tpd_connection *l2c = fi->priv;
+
 	switch (event) {
 	case L2CC_E_LOCAL_CLOSE_REQ:
 		/* FIXME: Send StopCCN */
 		break;
 	case L2CC_E_RX_STOP_CCN:
-		/* FIXME */
+		l2tp_tx_ack(l2c);
+		/* FIXME: tear down whole l2c */
 		break;
 	}
 }
