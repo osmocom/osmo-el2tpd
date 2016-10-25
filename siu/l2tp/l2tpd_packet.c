7471a6c65dee8dffec9af40cbdd04da0e560738d
Alexander Couzens <lynxis@fe80.eu>
2016-10-25 15:15:13 +0200

    l2tpd_packet: improve log messages for rx_ic_rq

--- siu/l2tp/l2tpd_packet.c
+++ siu/l2tp/l2tpd_packet.c
@@ -553,16 +553,24 @@ static int rx_ic_rq(struct l2tpd_connection *l2cc, struct msgb *msg, struct avps
 
 	if (!l2cc)
 		return -1;
-	if (avpp_val_u32(ap, VENDOR_IETF, AVP_IETF_REM_SESS_ID, &r_sess_id))
+	if (avpp_val_u32(ap, VENDOR_IETF, AVP_IETF_REM_SESS_ID, &r_sess_id)) {
+		LOGP(DL2TP, LOGL_ERROR, "ccid %d: Missing AVP REM_SESS_ID\n",
+		     l2cc->local.ccid);
 		return -1;
-	if (avpp_val_u32(ap, VENDOR_IETF, AVP_IETF_LOC_SESS_ID, &l_sess_id))
+	}
+	if (avpp_val_u32(ap, VENDOR_IETF, AVP_IETF_LOC_SESS_ID, &l_sess_id)) {
+		LOGP(DL2TP, LOGL_ERROR, "ccid %d: Missing AVP LOC_SESS_ID\n",
+		     l2cc->local.ccid);
 		return -1;
+	}
 
 	if (r_sess_id == 0) {
 		l2s = l2tpd_sess_alloc(l2cc);
 		l2s->r_sess_id = l_sess_id;
 		avpp_val_u16(ap, VENDOR_IETF, AVP_IETF_PW_TYPE, &l2s->pw_type);
 	} else {
+		LOGP(DL2TP, LOGL_NOTICE, "ccid %d: Received rx_ic_rq for already known session %u\n",
+		     l2cc->local.ccid, r_sess_id);
 		l2s = l2tpd_sess_find_by_l_s_id(l2cc, r_sess_id);
 		if (!l2s) {
 			LOGP(DL2TP, LOGL_ERROR, "NoSession %u\n",
