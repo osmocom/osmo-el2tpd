53c8999df1a5e5e5854b9b1b5e3b896cc0532307
Alexander Couzens <lynxis@fe80.eu>
2016-10-31 00:41:18 +0100

    l2tpd: move next_l_sess_id into l2tp_instance
    the sessions are unique to the daemon. E.g. data packets only contain a
    session id and no connection id

--- siu/l2tp/l2tpd_data.h
+++ siu/l2tp/l2tpd_data.h
@@ -16,7 +16,7 @@ l2tpd_cc_alloc(struct l2tpd_instance *inst);
 
 /* l2tp session */
 struct l2tpd_session *
-l2tpd_sess_alloc(struct l2tpd_connection *conn);
+l2tpd_sess_alloc(struct l2tpd_instance *inst, struct l2tpd_connection *conn);
 
 struct l2tpd_session *
 l2tpd_sess_find_by_l_s_id(struct l2tpd_connection *conn, uint32_t session_id);
